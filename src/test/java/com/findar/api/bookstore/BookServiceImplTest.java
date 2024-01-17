0cd13b737edff2528266f1974e11068679eae671
Ifeanyi Osuji <oindubuisi@gmail.com>
2024-01-17 10:10:07 +0100

    Corrected the test errors in BookServiceImpltest class

--- src/test/java/com/findar/api/bookstore/BookServiceImplTest.java
+++ src/test/java/com/findar/api/bookstore/BookServiceImplTest.java
@@ -2,12 +2,18 @@ package com.findar.api.bookstore;
 
 import com.findar.api.bookstore.dto.request.BookRequest;
 import com.findar.api.bookstore.dto.response.BookResponse;
+import com.findar.api.bookstore.entities.Book;
 import com.findar.api.bookstore.exceptions.BookAlreadyExistException;
+import com.findar.api.bookstore.exceptions.BookNotFoundException;
 import com.findar.api.bookstore.repository.BookRepository;
 import com.findar.api.bookstore.services.BookServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,17 +22,22 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BookServiceImplTest {
     @Autowired
     private BookServiceImpl bookService;
-    @Autowired
+@Autowired
     private BookRepository repository;
     @Test
+    @BeforeEach
     void contextLoads() {
+
     }
+
+
     @Test
+    @DirtiesContext
     public void testBookCanBeAdded() throws BookAlreadyExistException {
         //given
         BookRequest request = new BookRequest(
-                "Biology",
-                "Osuji",
+                "Physics",
+                "Sam",
                 "isbn:oiuuyttrt",
                 "textbook",
                 "2010",
@@ -61,6 +72,48 @@ public class BookServiceImplTest {
         assertThrows(BookAlreadyExistException.class,()->{
             bookService.addBook(request);
         });
+
+    }
+    @Test
+    void testBookCanBeUpdated() throws BookNotFoundException {
+        //given
+        BookRequest request = new BookRequest(
+                "Biology",
+                "Osuji",
+                "isbn:oiuuyttrt",
+                "textbook",
+                "2010",
+                200.00,
+                10
+
+        );
+        BookRequest requestToUpdate = new BookRequest(
+                "Biology",
+                "Osuji",
+                "isbn:oiuuyttrt",
+                "textbook",
+                "2010",
+                500.00,
+                20
+        );
+        Book book = new Book(
+                request.getTitle(),
+                request.getAuthor(),
+                request.getIsbn(),
+                request.getGenre(),
+                request.getPublicationYear(),
+                request.getPrice(),
+                request.getQuantityInStock(),
+                new Date(),
+                new Date()
+
+        );
+        //when
+        Book bookToSave = repository.save(book);
+        BookResponse updatedBook = bookService.updateBookDetails(bookToSave.getId(), requestToUpdate);
+
+        //then
+        assertThat(updatedBook).isNotNull();
 
     }
 
